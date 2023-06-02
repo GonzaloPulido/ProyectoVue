1f86c42de305a27945aa1d066de8225c987f3172
GonzaloPulido <gpulido.alumno@iesrafaelalberti.es>
2023-06-02 19:28:34 +0200

    api

--- src/pages/Pilotos.vue
+++ src/pages/Pilotos.vue
@@ -1,5 +1,29 @@
 <script>
-import {fetchDataFromApi} from '../helpers/apiHelper'
+import { fetchDataFromApi } from '../helpers/apiHelper';
+
+export default {
+  data() {
+    return {
+      apiUrl: 'https://apimotogp2023-production.up.railway.app/api/ridersMotoGP' // URL que deseas pasar como parámetro
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const data = await fetchDataFromApi(this.apiUrl);
+        // Utiliza los datos obtenidos de la API como desees
+        console.log(data);
+      } catch (error) {
+        // Maneja el error de manera adecuada
+        console.error(error);
+      }
+    }
+  }
+  
+}
 </script>
 <template>
     <div>Bienvenido a pilotos</div>
