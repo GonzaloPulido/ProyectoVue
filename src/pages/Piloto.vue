741d785d15448de254fa87f137cee6619c113ba2
GonzaloPulido <gpulido.alumno@iesrafaelalberti.es>
2023-06-16 10:31:52 +0200

    Estilo Piloto

--- src/pages/Piloto.vue
+++ src/pages/Piloto.vue
@@ -33,16 +33,53 @@ export default {
         <img :src="this.riderObject.image" alt="Imagen del piloto" class="riderimage" />
         <div class="inforider">
         <p class="ridername">{{ this.riderObject.name }}</p>
-        <p>{{ this.riderObject.country }}</p>
-        <p>{{ this.riderObject.height }}</p>
-        <p>{{ this.riderObject.dateOfBirth }}</p>
-        <p>{{ this.riderObject.bike }}</p>
-        <p>{{ this.riderObject.team }}</p>
-        <p>{{ this.riderObject.poles }}</p>
-        <p>{{ this.riderObject.worldChampionships }}</p>
+        <p>Pais: {{ this.riderObject.country }}</p>
+        <p>Altura: {{ this.riderObject.height }} cm</p>
+        <p>Fecha de nacimiento: {{ this.riderObject.dateOfBirth }}</p>
+        <p>Moto: {{ this.riderObject.bike }}</p>
+        <p>Equipo: {{ this.riderObject.team }}</p>
+        <p>Poles en la categoria: {{ this.riderObject.poles }}</p>
+        <p>Mundiales totales: {{ this.riderObject.worldChampionships }}</p>
       </div>
     </div>
 </template>
-<style>
-    
+
+<style scoped>
+  .containerrider{
+    color: white;
+    flex-wrap: wrap;
+    margin: auto;
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem;
+    margin-top: 50px;
+    background: rgb(0,0,0);
+    background: linear-gradient(0deg, rgba(0,0,0,1) 50%, rgba(255,255,255,0) 100%);
+    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+    margin-bottom: 100px;
+  }
+  .riderimage{
+    width: 50%;
+  }
+  .inforider{
+    margin-top: 50%;
+  }
+  @media (max-width: 1100px) {
+  .containerrider {
+    flex-direction: column;
+    margin: auto;
+    margin-bottom: 100px;
+  }
+  .riderimage{
+    width: 80%;
+    margin: auto;
+  }
+  .inforider{
+    margin: auto;
+    margin-top: 30px;
+  }
+}
 </style>
